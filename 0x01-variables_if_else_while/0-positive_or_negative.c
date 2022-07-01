f5921de9753048810db1265b54f074575e83f4e2
Abdulkadir <uabdulkadir06@gmail.com>
2022-07-01 13:18:30 +0000

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+int n;
+if (a > 0)
+{
+	printf("is positive%d\n", n);
+}
+else if (n == 0)
+{
+printf("is zero%d\n", n);
+}
+else
+{
+	printf("is negative%d\n", n);
+}
+return (0);
+
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This project tasks consist of problems on Variables, if, else and while in C programming.
