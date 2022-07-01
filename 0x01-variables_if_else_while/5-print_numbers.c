33fc4b50848074d0115cc05b12924fe087110fd4
Abdulkadir <uabdulkadir06@gmail.com>
2022-07-01 20:26:53 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* code description
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int k = 0;
+
+	if (k <= 10)
+{
+	printf("%d\n", k);
+	k++;
+}
+	return (0);
+}
