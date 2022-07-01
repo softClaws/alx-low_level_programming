c28d485b862ac476140f7dbc1373050ff7713739
Abdulkadir <uabdulkadir06@gmail.com>
2022-07-01 21:18:56 +0000

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* code description
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int f = 0;
+
+	for (; f < 10; f++)
+{
+	putchar(f);
+}
+	return (0);
+}
