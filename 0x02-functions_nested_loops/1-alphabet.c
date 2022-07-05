c9fbc3c97f98bec94771fa3a21f527b640a5a302
Abdulkadir <uabdulkadir06@gmail.com>
2022-07-05 22:24:11 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* main - Entry point
+* code description
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	char j;
+
+	j = 'a';
+
+	while (j <= 'z')
+	{
+	_putchar(j);
+	j++;
+	}
+	_putchar('\n');
+}
