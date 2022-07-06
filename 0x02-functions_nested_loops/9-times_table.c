e52de50bd13076260f64698d1fcd44e5d86a9fd7
Abdulkadir <uabdulkadir06@gmail.com>
2022-07-06 15:20:11 +0000

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - print multiplication table
+ */
+
+void times_table(void)
+{
+	int r, j;
+
+	for (r = 0; r <= 9; r++)
+
+	{
+
+	j = r * 9;
+
+	_putchar(j);
+	_putchar('\n');
+
+
+	}
+}
