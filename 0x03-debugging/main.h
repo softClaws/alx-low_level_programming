4be92839ab456aee2c1569837ab416dc772b6ecd
Abdulkadir <uabdulkadir06@gmail.com>
2022-07-07 11:08:05 +0000

    task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,29 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include "main.h"
 
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+
+int main(void)
+{
+	int a, b, c;
+	int largest;
+
+	a = 972;
+	b = -98;
+	c = 0;
+
+	largest = largest_number(a, b, c);
+
+	printf("%d is the largest number\n", largest);
+
+	return (0);
+}
