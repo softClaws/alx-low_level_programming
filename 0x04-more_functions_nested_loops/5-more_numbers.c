9dca0d166c21037dd187e2ac47c7057ff66d1051
Abdulkadir <uabdulkadir06@gmail.com>
2022-07-07 23:06:08 +0000

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times number(1-14)
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	char a;
+	int b;
+
+	for (a = '0'; a <= '14'; a++)
+	{
+		b = 10 * a
+
+		_putchar(b);
+		_putchar('\n');
+	}
+}
