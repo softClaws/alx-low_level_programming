538f8ab39fccfac610b4c07cd301235aa0f20ca8
Abdulkadir <uabdulkadir06@gmail.com>
2022-07-14 18:30:17 +0000

    advanced task 5

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,3 +10,5 @@ char *rot13(char *s);
 void print_number(int n);
 int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+void print_line(char *c, int s, int l);
