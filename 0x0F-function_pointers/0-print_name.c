39490d886541b5a7be69d49962e7568f280191ec
Abdulkadir <uabdulkadir06@gmail.com>
2022-08-03 14:31:17 +0000

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_name - function to print name
+ * @name: argument one
+ * @f: argument two
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
