95774b5b8fc91fa8c29365a3ca7a75845dfb1619
Abdulkadir <uabdulkadir06@gmail.com>
2022-08-15 18:20:38 +0000

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,7 +8,16 @@
 
 void free_listint(listint_t *head)
 {
+	listint_t *ptr;
+
+	if (head == NULL)
+		return;
+
 	if (head != NULL)
-		free(head);
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
 
 }
