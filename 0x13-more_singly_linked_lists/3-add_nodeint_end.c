20247e03cf2edc2979d818a5f99e3f3647533a7e
Abdulkadir <uabdulkadir06@gmail.com>
2022-08-15 17:53:49 +0000

    checking checker

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,28 +11,29 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *newnode, *currentnode;
 
-	if (head == NULL)
-		return (NULL);
 
-	newnode = malloc(sizeof(listint_t));
+	newnode = malloc(sizeof(listint_t)); /*create new node*/
 
 	if (newnode == NULL)
 		return (NULL);
 
+	/*setting value and pointer for new node*/
 	newnode->n = n;
 	newnode->next = NULL;
 
-	if (*head == NULL)
+	if (*head == NULL) /*if list is empty*/
 	{
 		*head = newnode;
-		return (newnode);
 	}
-	currentnode = *head;
-	while (currentnode->next != NULL)
+	else
 	{
-	currentnode = currentnode->next;
+		currentnode = *head;
+		while (currentnode->next != NULL) /*keep looping till last nodee*/
+		{
+		currentnode = currentnode->next;
+		}
+		currentnode->next = newnode; /*linking last node to new node*/
 	}
-	currentnode->next = newnode;
 
 	return (newnode);
 
