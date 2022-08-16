393d01b1f0ca725721bef42e83a71da2dd77a226
Abdulkadir <uabdulkadir06@gmail.com>
2022-08-16 14:44:31 +0000

    lists.h error

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,7 +21,7 @@ typedef struct listint_s
 
 } listint_t;
 
-char _putchar(char *);
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
