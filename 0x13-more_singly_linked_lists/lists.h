16930f69cecdad40f7392f8f92f6f7ee0ef55f1e
Abdulkadir <uabdulkadir06@gmail.com>
2022-08-16 14:47:51 +0000

    header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,8 +10,9 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
- * for alx-holberton project
+ * 
  */
 
 typedef struct listint_s
